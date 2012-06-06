8df340a66d6f5a72b61b16c8bc1c522316bed5e5
Paul M Bauer <paul.michael.bauer@gmail.com>
2012-06-06 16:45:42 -0700

    project: bumped to latest clojure release

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject monads2 "1.0.0-SNAPSHOT"
   :description "A protocol based implementation of monads"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
