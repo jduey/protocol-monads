86a47f9180f6ce64494ed5f987cbfd627e27a1e2
Jim Duey <jim@intensivesystems.net>
2012-09-17 20:43:57 -0500

    Vers. 1.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject monads2 "1.0.0-SNAPSHOT"
+(defproject monads2 "1.0.0"
   :description "A protocol based implementation of monads"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
