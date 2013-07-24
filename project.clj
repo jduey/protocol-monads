2293a6045f6c9239d16a51a268a33f8862769994
Roman Gonzalez <romanandreg@gmail.com>
2013-07-23 18:56:47 -0700

    Update version of libraries

--- project.clj
+++ project.clj
@@ -3,39 +3,33 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :source-paths ["src/clj", "src/cljs"]
   :test-paths ["test/clj"]
-  :profiles {:dev {:dependencies [[com.birdseye-sw/buster-cljs "0.1.0"]]
-                   :plugins [[lein-cljsbuild "0.2.9"]
-                             [com.birdseye-sw/lein-dalap "0.1.0"]]}}
+  :profiles {:dev {:dependencies [[com.birdseye-sw/buster-cljs "0.1.2"]]
+                   :plugins [[lein-cljsbuild "0.3.2"]
+                             [com.birdseye-sw/lein-dalap "0.1.1"]]}}
   :hooks [leiningen.dalap]
 
   :cljsbuild
   {:builds
-   [{:id :dev
-     :source-path "src/cljs"
-     :compiler
-     {:output-to "resources/js/protocol_monads_dev.js"
-      :optimizations :simple
-      :pretty-print true}}
-    {:id :browser-test
-     :source-path "test/cljs"
+   [{:id :browser-test
+     :source-paths ["src/cljs" "test/cljs"]
      :compiler
      {:output-to "resources/js/protocol_monads_browser_test.js"
-      :libraries ["resources/js/protocol_monads_dev.js"]
+      :target :browser
       :externs ["externs/buster.js"]
       :optimizations :simple
       :pretty-print true}}
     {:id :browser-test-optimized
-     :source-path "test/cljs"
+     :source-path ["src/cljs" "test/cljs"]
      :compiler
      {:output-to "resources/js/protocol_monads_browser_optimized_test.js"
-      :libraries ["resources/js/protocol_monads_dev.js"]
+      :target :browser
       :externs ["externs/buster.js"]
       :optimizations :advanced}}
     {:id :node-test
-     :source-path "test/cljs"
+     :source-paths ["src/cljs" "test/cljs"]
      :compiler
      {:output-to "resources/js/protocol_monads_node_test.js"
-      :libraries ["resources/js/protocol_monads_dev.js"]
+      :target :node
       :externs ["externs/buster.js"]
       :optimizations :simple
       :pretty-print true}}]}
