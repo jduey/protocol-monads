4a00113b7c36dad114af5acd6e1d47124e0ddbf8
Jim Duey <jim@intensivesystems.net>
2013-10-06 17:22:40 -0500

    Added satsifies? check to state-transformer

--- src/clj/monads/core.clj
+++ src/clj/monads/core.clj
@@ -12,7 +12,6 @@
   (:require [clojure.set :as set]))
 
 (defprotocol Monad
-  (has-zero? [_])
   (do-result [_ v])
   (bind [mv f]))
 
@@ -352,8 +351,6 @@
       (c v)))
 
   Monad
-  (has-zero? [_]
-    false)
   (do-result [_ v]
     (cont-monad. v nil nil))
   (bind [mv f]
@@ -421,12 +418,12 @@
   clojure.lang.IFn
   (invoke [_ s]
     (cond
-     alts (if (has-zero? (m nil))
+     alts (if (satisfies? MonadZero m)
             (plus (clojure.core/map #(% s) alts)))
      f (bind (mv s)
              (fn [[v ss]]
                ((f v) ss)))
-     :else (if (and (has-zero? (m nil))
+     :else (if (and (satisfies? MonadZero m)
                     (= v (zero (m nil))))
              v
              (m [v s]))))
